f5ad5b88e2c91839c8b413b1d1edfecacbe2b87c
bhanu3450 <bhanu3450@gmail.com>
2023-09-24 23:55:01 +0530

    Added menu

--- Assignment3_Q1.cpp
+++ Assignment3_Q1.cpp
@@ -1,49 +1,85 @@
-// Q1. Write a menu driven program to calculate volume of the box(length * width * height).
-// Provide necessary constructors.
-
-#include <iostream>
-#include <string>
+#include<iostream>
 using namespace std;
 
+class volumeOfBox{
+    private:
+    double length;
+    double width;
+    double height;
+    public:
+    volumeOfBox(){
+        this->length=0;
+        this->height=0;
+        this->width=0;
+    }
+    volumeOfBox(double length ){
+        this->length = length;
+        this->width = 0;
+        this->height = 0;
+    }
+    volumeOfBox(double length , double width ){
+        this->length = length;
+        this->width = width;
+        this->height = 0;
+    }
+    volumeOfBox(double length , double width,  double hgth ){
+        this->length = length;
+        this->width = width;
+        this->height = height;
+    }
+    void accept(){
+        cout << "please enter length : ";
+        cin >> this->length;
+        cout << "please enter width : ";
+        cin >> this->width;
+        cout << "please enter height : ";
+        cin >> this->height;
+        
+        }
+    void display(){
+        cout << "length : "<<length<<endl;
+        cout << "Width : "<<width<<endl;
+        cout << "Hight : "<<height<<endl;
+    }
 
+    void volume(){
+        cout << "THE VOLUME OF THE BOX IS : " << length*width*height << endl;
+    }
 
-int volumeOfBox(){
-    int length;
-    int width; 
-    int height;
-    cout << "enter length, width, height";
-    cin >> length >> width >> height;
-    return length*width*height;
-}
+};
 
-int  input()
-{
-    int num;
-    cout << "*******************" << endl;
-    cout << "0. EXIT" << endl;
-    cout << "1.volume calculator" << endl;
-    cout << "Enter your choice = ";
-    cin >> num;
-    cout << "*******************" << endl;
-    return num;
+int menu(){
+    int choice;
+    cout << "****************************************" << endl ;
+    cout << "0. Exit" << endl ;
+    cout << "1. Enter dimensions " << endl ;
+    cout << "2. Display dimensions" << endl ;
+    cout << "3. Calculate and show Volume" << endl ;
+    cout << "Please enter your choice : " ;
+    cin >> choice ;
+    cout << "****************************************" << endl ;
+    return choice;
 }
 
 int main(){
-while (1)
-{
-    int choice = input();
-    switch (choice)
+    volumeOfBox b1;
+    int choice;
+    while(choice = menu())
     {
-    case 0:
-        cout << "Exitttingggggg goodbye" << endl;
-        exit(0);
-
-    case 1:
-             int result = volumeOfBox();
-            cout << "calculated area is : " << result << endl;
-        break;
-    
+        switch (choice)
+        {
+        case 1: b1.accept();
+            break;
+     
+        case 2: b1.display();
+            break;
+     
+        case 3: b1.volume();
+            break;
+     
+        default:
+        cout << "PLEASE ENTER A VALID INPUT " << endl;
+            break;
+        }
     }
-};
 }
-
